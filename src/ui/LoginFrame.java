beafd5c80bcb783d18e0c8ca2bf5585ab13fc108
Danidu200523 <ddaniduherath353@gmail.com>
2026-02-13 23:56:36 +0530

    Set up Java project structure and implemented initial LoginFrame

---
+++ src/ui/LoginFrame.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javax.swing.JFrame;
+
+public class LoginFrame extends JFrame {
+
+    public LoginFrame() {
+        setTitle("Login");
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
